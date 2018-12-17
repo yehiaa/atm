4c13035cd8384e3d29ac1851b38f46cc8ff878d5
Yehia AbdulHamid <yehiamcp@gmail.com>
2018-12-17 03:59:06 +0200

    fixed title

--- application/resources/views/auth/login.blade.php
+++ application/resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'ATM, by Agilia systems') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
