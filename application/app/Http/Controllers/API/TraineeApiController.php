a1439aa4d4141f96f1f222f469c88f0788a1377d
Yehia AbdulHamid <yehiamcp@gmail.com>
2019-02-15 10:32:46 +0200

    moved controller

--- application/app/Http/Controllers/API/TraineeApiController.php
+++ application/app/Http/Controllers/API/TraineeApiController.php
@@ -7,26 +7,37 @@ use Illuminate\Http\Request;
 
 class TraineeApiController extends Controller
 {
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function index(Request $request)
     {
+        dump($request);
+        dump($_REQUEST);
+        dump($_POST);
+        dump($_GET);
+        dump($request->q);
+//        exit('');
 //        $q = request('q');
         $all = $request->all();
-        $q = $request->json('q');
+//        $q = $request->json('q');
+        $q = $request->q;
 //        $q = $request->get('q');
-//        $qq = $request->query('q');
+        $qq = $request->query('q');
 //        $qqq = $request->input('q');
 //        $term = Input::get('term');
 //        dd([$all ,$q, $qq, $qqq]);
 //        dd([$q]);
         $items = [];
 //        $items = Trainee::all();
-//        if ($q){
+        if ($q){
             $items = Trainee::where('name', 'like', '%'.$q.'%')
                 ->orWhere('phone', '%'.$q.'%')
                 ->orWhere('identity', '%'.$q.'%')
                 ->orWhere('email', '%'.$q.'%')
                 ->get();
-//        }
+        }
         return response()->json($items);
     }
 }
