59d5a52bbcd1ae5252e71866d2ae4bf4720941d1
Yehia AbdulHamid <yehiamcp@gmail.com>
2019-03-23 20:41:23 +0200

    added relation to course trainer

--- application/app/CourseTrainer.php
+++ application/app/CourseTrainer.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 class CourseTrainer extends Pivot
 {
     protected $fillable = ['course_id', 'trainer_id'];
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+
+    public function trainer()
+    {
+        return $this->belongsTo(Trainer::class);
+    }
 }
