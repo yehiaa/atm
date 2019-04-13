6742fed6918afe74742f91f2f37fe369b7ac251c
NeMoos <nabil.nr123@gmail.com>
2019-04-13 18:29:58 +0200

    repair Auth

--- application/app/Http/Kernel.php
+++ application/app/Http/Kernel.php
@@ -19,8 +19,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
-        \App\Http\Middleware\AdminMiddleware::class,
-        \App\Http\Middleware\ClearanceMiddleware::class,
+
     ];
 
     /**
@@ -37,6 +36,8 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\AdminMiddleware::class,
+            \App\Http\Middleware\ClearanceMiddleware::class,
         ],
 
         'api' => [
