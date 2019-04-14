45ac7f3bf00143fd2530449ffb1466349f892dc5
Yehia AbdulHamid <yehiamcp@gmail.com>
2019-04-14 13:40:52 +0200

    updated middlewares

--- application/app/Http/Controllers/UserController.php
+++ application/app/Http/Controllers/UserController.php
@@ -11,8 +11,8 @@ class UserController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth', 'isAdmin']); //isAdmin middleware lets only users with a //specific permission permission to access these resources
-
+        //isAdmin middleware lets only users with a //specific permission permission to access these resources
+//        $this->middleware(['auth', 'isAdmin']);
     }
 
     /**
--- application/app/Http/Kernel.php
+++ application/app/Http/Kernel.php
@@ -19,8 +19,6 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
-        \App\Http\Middleware\AdminMiddleware::class,
-        \App\Http\Middleware\ClearanceMiddleware::class,
     ];
 
     /**
@@ -37,6 +35,8 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            // \App\Http\Middleware\AdminMiddleware::class,
+            // \App\Http\Middleware\ClearanceMiddleware::class,
         ],
 
         'api' => [
@@ -62,6 +62,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'isAdmin' => \App\Http\Middleware\AdminMiddleware::class,
     ];
 
     /**
