f839ad0c5a6b7c1af7c6010ecb40cf46af655e7b
Yehia AbdulHamid <yehiamcp@gmail.com>
2019-04-22 03:01:07 +0200

    added demo message using middleware

--- application/app/Http/Middleware/ClearanceMiddleware.php
+++ application/app/Http/Middleware/ClearanceMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class ClearanceMiddleware {
     /**
@@ -16,6 +17,9 @@ class ClearanceMiddleware {
     public function handle($request, Closure $next)
     {
         $classActionName = class_basename($request->route()->getActionName());
+
+        Session::flash('warning', 'For DEMO ONLY Don\'t use in production..');
+
         $sections = explode('@', $classActionName);
 
         //based on controller name
